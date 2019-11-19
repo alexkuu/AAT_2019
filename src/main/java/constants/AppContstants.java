b84fbca00564d9d8e96c8e44f700f97a529751ae
Aliaksandr Kukharenka <aliaksandr_kukharenka@epam.com>
2019-11-19 12:22:50 +0300

    Commit

--- src/main/java/constants/AppContstants.java
+++ src/main/java/constants/AppContstants.java
@@ -5,7 +5,7 @@ public class AppContstants {
     public static final String DEMO_DASHBOARD_PREFIX = "_demo";
 
     // Widgets
-    public static final String WIDGET_TOP_ID = "5dcbca32bfe52300018f2cc1";
-    public static final String WIDGET_BOTTOM_ID = "5dcbca32bfe52300018f2cc9";
-    public static final String WIDGET_OVERALLSTATISTICS_ID = "5dcbca32bfe52300018f2cc4";
+    public static final String WIDGET_TOP_ID = "5dd3afabbfe5230001f54b40";
+    public static final String WIDGET_BOTTOM_ID = "5dd3afabbfe5230001f54b48";
+    public static final String WIDGET_OVERALLSTATISTICS_ID = "5dd3afabbfe5230001f54b43";
 }
