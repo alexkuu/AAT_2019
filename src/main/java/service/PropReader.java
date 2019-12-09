834d0d286db5fe9cb1f1ce7bfce6dff85e9d939f
Aliaksandr Kukharenka <aliaksandr_kukharenka@epam.com>
2019-12-09 13:18:45 +0300

    Commit

--- src/main/java/service/PropReader.java
+++ src/main/java/service/PropReader.java
@@ -1,17 +1,15 @@
 package service;
 
-import com.sun.jndi.toolkit.url.Uri;
-
-import java.io.Console;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class PropReader {
 
-    private String content;
+    private ArrayList<String> content = new ArrayList<>();
 
     PropReader(String fileName) {
         try {
@@ -21,16 +19,31 @@ public class PropReader {
                     .replace("var/jenkins_home/workspace/PR-check_testBranch@script/", "")
                     .replace("var/jenkins_home/workspace/PR-check_testBranch/", "");
             System.out.println("Decoded: " + stringPath);
-            File file = Paths.get(stringPath).toFile();
-            content = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(new File(stringPath)));
+                String line, name, email;
+                // read through the first two lines to get to the data
+                line = br.readLine();
+                line = br.readLine();
+                while ((line = br.readLine()) != null) {
+                    if (line.contains("=")) {
+                        content.add(line);
+                    }
+                }
+                br.close();
+            } catch (Exception e) {
+                System.out.println("There was an issue parsing the file.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     String readProperty(String propertyName) {
-        for (String line : content.split("\r\n")) {
-            System.out.println("Reading property debug: Searching ["+propertyName+"]. LINE: " + line);
+        for (String line : content) {
+            System.out.println("Reading property debug: Searching [" + propertyName + "]. LINE: " + line);
             System.out.println("Current line property is: [" + line.split("=")[0].trim() + "]");
             if (line.split("=")[0].trim().equals(propertyName)) {
                 return line.split("=")[1];
