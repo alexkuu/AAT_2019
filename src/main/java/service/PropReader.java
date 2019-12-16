70eaea2bec0ee2ef5559658511e61f7e8332591d
Aliaksandr Kukharenka <aliaksandr_kukharenka@epam.com>
2019-12-16 11:45:43 +0300

    Commit

--- src/main/java/service/PropReader.java
+++ src/main/java/service/PropReader.java
@@ -15,14 +15,13 @@ public class PropReader {
         try {
             URL res = PropReader.class.getClassLoader().getResource(fileName);
             System.out.println("Reading file:" + res.getPath());
-            String stringPath = URLDecoder.decode(res.toString(), "UTF-8").replace("file:/", "")
-                    .replace("var/jenkins_home/workspace/PR-check_testBranch@script/", "")
-                    .replace("var/jenkins_home/workspace/PR-check_testBranch/", "");
-            System.out.println("Decoded: " + stringPath);
-
+//            String stringPath = URLDecoder.decode(res.toString(), "UTF-8").replace("file:/", "")
+//                    .replace("var/jenkins_home/workspace/PR-check_testBranch@script/", "")
+//                    .replace("var/jenkins_home/workspace/PR-check_testBranch/", "");
+//            System.out.println("Decoded: " + stringPath);
 
             try {
-                BufferedReader br = new BufferedReader(new FileReader(new File(stringPath)));
+                BufferedReader br = new BufferedReader(new FileReader(new File(res.getPath().replace("file:", ""))));
                 String line, name, email;
                 // read through the first two lines to get to the data
                 line = br.readLine();
